6e7e6b27d2a438bea45334bbda3e62759a011e31
tiya <tkbarot@gmail.com>
2025-12-05 16:48:47 +0000

    make mongodb script

---
+++ MongoDB.sh
@@ -0,0 +1,10 @@
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+
+dnf install mongodb-org -y
+
+sed - i  's/127.0.0.1 /0.0.0.0'/etc/mongo.conf
+systemctl enable mongod
+systemctl start mongod
+
+systemctl restart mongod
